fcd2485e79b3d4d9252266098fa3290113e83a3c
eminsuleymanov <eminyscode.edu.az>
2023-05-08 03:27:24 +0400

    category crud and material crud added

--- LuminateFinalProject/Models/Material.cs
+++ LuminateFinalProject/Models/Material.cs
@@ -7,7 +7,7 @@ namespace LuminateFinalProject.Models
     {
         [StringLength(255)]
         public string Name { get; set; }
-        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Product>? Products { get; set; }
     }
 }
 
